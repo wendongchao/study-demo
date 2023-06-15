e8e64ed1db5a19234b45573d8d1495942fc42cca
wendongchao <dongchao1860369@sina.com>
2023-06-15 16:34:22 +0800

    io流基础

--- java-demo/src/test/java/com/example/io/TestIO.java
+++ java-demo/src/test/java/com/example/io/TestIO.java
@@ -5,6 +5,8 @@ import junit.framework.TestCase;
 import java.io.*;
 
 /**
+ * io流基础
+ * 有缓冲实现可以更快实现传输，可以验证下
  * @auther wendongchao
  * @date 2023/6/15 16:13
  **/
@@ -18,7 +20,7 @@ public class TestIO extends TestCase {
      */
     public void test01() throws IOException {
         //1.实例化File类的对象
-        File file = new File("../doc/aaa.txt");
+        File file = new File("../doc/io.txt");
         //2.提供具体的流
         FileReader fr = new FileReader(file);
         //3.数据的读入
@@ -38,7 +40,7 @@ public class TestIO extends TestCase {
      * @throws IOException
      */
     public void test02() throws IOException {
-        File file = new File("../doc/aaa.txt");
+        File file = new File("../doc/io.txt");
         FileWriter fw = new FileWriter(file);
         String data = "io writer";
         char[] charArray = data.toCharArray();
@@ -53,7 +55,7 @@ public class TestIO extends TestCase {
      * @throws IOException
      */
     public void test03() throws IOException {
-        InputStream fileInputStream = new FileInputStream("../doc/aaa.txt");
+        InputStream fileInputStream = new FileInputStream("../doc/io.txt");
 
         int data = fileInputStream.read();
         while (data != -1) {
@@ -68,7 +70,7 @@ public class TestIO extends TestCase {
      * @throws IOException
      */
     public void test04() throws IOException {
-        OutputStream fileOutputStream = new FileOutputStream("../doc/aaa.txt");
+        OutputStream fileOutputStream = new FileOutputStream("../doc/io.txt");
 
         String data = "io outputStream";
         byte[] bytes = data.getBytes();
